041c3706d031db3cfde97c06b59ef4463bd4737c
msg8925 <msg.8925@gmail.com>
2022-11-22 13:33:38 +0700

    Text encrypt/decrypt under construction

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import rsa
 
-
-
 if __name__=="__main__":
+
+    original_msg = []
+    c = []
+
     keys = rsa.rsa_generate_key(61, 53)
 
     print(f"Keys={keys}")
@@ -13,17 +15,30 @@ if __name__=="__main__":
     print(f"Public key = {public_key}")
     print(f"Private key = {private_key}")
 
-    # encrypt data
-
-    # Convert char to ascii value
-    user_char = input("Please enter a char: ")
-    msg = ord(user_char)
-    print(f"msg = {msg}")
     e = public_key[0]
     n = public_key[1]
-    c = rsa.rsa_encrypt(msg, e, n)
+
+    # Convert char to ascii value
+    #user_char = input("Please enter a char: ")
+    user_string = input("Please enter a string: ")
+    user_string.split() 
+    print(f"user_string = {user_string}")
+
+    for msg in user_string: 
+        msg = ord(msg)
+        print(f"msg = {msg}")
+        
+        c.append(rsa.rsa_encrypt(msg, e, n))
+
+    print(f"Encrypted message: {c}")
 
     # decrypt data
-    original_msg = 0
-    d = private_key[0]
-    original_msg = rsa.rsa_decrypt(c, d, n)
+    
+
+    # d = private_key[0]
+    
+    # for original_char in original_msg:
+    #     original_msg.append(rsa.rsa_decrypt(c, d, n))
+
+
+    # print(f"original message = {original_msg}")    
